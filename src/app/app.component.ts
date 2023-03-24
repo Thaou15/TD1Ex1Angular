7d44f72cb2d70bced65735243c433a86d45664cc
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-24 14:35:40 +0100

    Ajout du tableau avec style css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,30 @@ export class AppComponent {
   nom:string = "Jean";
   verif:boolean=true;
   elem:number=1;
+
   etudiants: any[] = [
     {"nom":"Dupret", "prenom":"Victoria", "age":18},
     {"nom":"Delfino", "prenom":"Victor", "age":20},
     {"nom":"Durant", "prenom":"Louis", "age":16},
-    {"nom":"Demont", "prenom":"Lucas", "age":22}
-
-    
+    {"nom":"Demont", "prenom":"Lucas", "age":22}   
   ]
+
+  choixClass(isFirst:boolean, isLast:boolean, isEven:boolean)
+  {
+    var ma_classe:string="";
+    if(isFirst || isLast)
+    {
+      ma_classe = "first_last";
+    }
+    if(isEven)
+    {
+      ma_classe +="ligne_even";
+    }
+    else
+    {
+      ma_classe += "ligne_odd";
+    }
+    return ma_classe;
+  }
 
 }

c374720e991e840aa83a1fbd0507d6744bd34cb5
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-24 12:08:37 +0100

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TD1EX1';
+  title = 'Mon application';
+  nom:string = "Jean";
+  verif:boolean=true;
+  elem:number=1;
+  etudiants: any[] = [
+    {"nom":"Dupret", "prenom":"Victoria", "age":18},
+    {"nom":"Delfino", "prenom":"Victor", "age":20},
+    {"nom":"Durant", "prenom":"Louis", "age":16},
+    {"nom":"Demont", "prenom":"Lucas", "age":22}
+
+    
+  ]
+
 }
